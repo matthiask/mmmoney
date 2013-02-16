2480762607a19f5ec2e58f697442788defc317aa
Matthias Kestenholz <mk@spinlock.ch>
2013-02-16 14:04:24 +0100

    gunicorn and psycopg2 are not necessary for development

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ def deploy():
         run('git checkout master') # yes.
         run('git pull')
         run('find . -name "*.pyc" -delete')
-        run('venv/bin/pip install -r requirements.txt')
+        run('venv/bin/pip install -r requirements/live.txt')
         run('venv/bin/python manage.py migrate')
         run('venv/bin/python manage.py syncdb')
         #with cd('tomac/static/tomac'):
