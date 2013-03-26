27c5034cd3d9f49888a6bd2439dcc7ca993c9de8
Matthias Kestenholz <mk@spinlock.ch>
2013-03-26 15:06:48 +0100

    Add staticfiles_urlpatterns for the development

--- mmmoney/urls.py
+++ mmmoney/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from mmmoney.views import entry_views
 
@@ -10,4 +11,4 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'', include(entry_views.urls)),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + staticfiles_urlpatterns()
