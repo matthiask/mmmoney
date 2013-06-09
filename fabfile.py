3a79a51cfb23030707d80b8976cd8db6993c895c
Matthias Kestenholz <mk@spinlock.ch>
2013-06-09 10:50:09 +0200

    Update the fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,42 @@
 from fabric.api import *
 
 
+HOST = 'deploy@mmmoney.406.ch'
+
+
 env.forward_agent = True
-env.hosts = ['mmmoney.406.ch']
+env.hosts = [HOST]
+
+
+@task(alias='ws')
+def watch_styles():
+    local('compass watch mmmoney/static/mmmoney')
+
+
+@task
+def deploy_styles():
+    local('compass clean mmmoney/static/mmmoney')
+    local('compass compile -s compressed mmmoney/static/mmmoney')
+    local('scp -r mmmoney/static/mmmoney/stylesheets %s:www/mmmoney/static/mmmoney/' % HOST)
 
 
 @task
 def deploy():
+    deploy_styles()
+
     local('git push origin master')
-    with cd('/var/www/mk/mmmoney'):
-        run('git checkout master') # yes.
-        run('git pull')
+    with cd('www/mk/mmmoney'):
+        run('git fetch')
+        run('git reset --hard origin/master')
         run('find . -name "*.pyc" -delete')
         run('venv/bin/pip install -r requirements/live.txt')
         run('venv/bin/python manage.py syncdb')
         run('venv/bin/python manage.py migrate')
-        with cd('mmmoney/static/mmmoney'):
-            run('GEM_HOME=/home/mk/_gems/ /home/mk/_gems/bin/compass compile -s compressed')
         run('venv/bin/python manage.py collectstatic --noinput')
-        run('sudo supervisorctl restart mk_mmmoney')
+        run('sudo service www-mk_mmmoney restart')
+
+
+@task
+def pyflakes():
+    # pip install pyflakes
+    local('pyflakes mmmoney/ | grep -v migrations')
