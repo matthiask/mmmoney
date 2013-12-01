5145d6e1f83500a84d80cc47cd7e3f6e049d48c9
Matthias Kestenholz <mk@spinlock.ch>
2013-12-01 15:25:16 +0100

    100 entries per page

--- mmmoney/resources.py
+++ mmmoney/resources.py
@@ -129,7 +129,7 @@ urlpatterns = patterns(
     '',
     entry_url(
         'list',
-        paginate_by=50,
+        paginate_by=100,
         search_form=EntrySearchForm,
         url=r'^$',
     ),
