f89b3d48857c8b4354172e9744e7a3f653ca13fb
Matthias Kestenholz <mk@spinlock.ch>
2013-12-01 14:16:21 +0100

    fabfile upgrade

--- fabfile.py
+++ fabfile.py
@@ -1,38 +1,97 @@
+import os
 from fabric.api import cd, env, local, run, task
 
 
-HOST = 'deploy@mmmoney.406.ch'
+CONFIG = {
+    'host': 'deploy@mmmoney.406.ch',
+    'project': 'mmmoney',
+    'branch': 'master',
+}
+
+
+CONFIG.update({
+    'sass': '{project}/static/{project}'.format(**CONFIG),
+    'service': 'www-mk_mmmoney',
+    'folder': 'www/mk/mmmoney/',
+})
 
 
 env.forward_agent = True
-env.hosts = [HOST]
+env.hosts = [CONFIG['host']]
+
+
+def _configure(fn):
+    def _dec(string, *args, **kwargs):
+        return fn(string.format(**CONFIG), *args, **kwargs)
+    return _dec
+
+
+local = _configure(local)
+cd = _configure(cd)
+run = _configure(run)
+
+
+@task
+def install(req):
+    if not os.path.isdir('venv'):
+        if os.path.exists('venv'):
+            print 'venv exists, but is not a folder. Aborting.'
+            return
+
+        local('virtualenv venv')
+
+    local('venv/bin/pip install -r requirements/{}.txt'.format(req))
+
+
+@task
+def dev():
+    import socket
+    from threading import Thread
+    jobs = [
+        Thread(target=watch_styles),
+        Thread(target=runserver),
+    ]
+    try:
+        socket.create_connection(('localhost', 6379), timeout=0.1).close()
+    except socket.error:
+        jobs.append(Thread(target=lambda: local('redis-server')))
+    [j.start() for j in jobs]
+    [j.join() for j in jobs]
 
 
 @task(alias='ws')
 def watch_styles():
-    local('compass watch mmmoney/static/mmmoney')
+    local('compass watch {sass}')
+
+
+@task(alias='rs')
+def runserver(port=8038):
+    local('venv/bin/python -Wall manage.py runserver 0.0.0.0:{}'.format(port))
 
 
 @task
 def deploy_styles():
-    local('compass clean mmmoney/static/mmmoney')
-    local('compass compile -s compressed mmmoney/static/mmmoney')
-    local(
-        'scp -r mmmoney/static/mmmoney/stylesheets'
-        ' %s:www/mk/mmmoney/static/mmmoney/' % HOST)
+    local('compass clean {sass}')
+    local('compass compile -s compressed {sass}')
+    local('scp -r {sass}/stylesheets {host}:{folder}static/{project}/')
 
 
 @task
-def deploy():
+def deploy_code():
     local('flake8 .')
-    deploy_styles()
-    local('git push origin master')
-    with cd('www/mk/mmmoney'):
+    local('git push origin {branch}')
+    with cd('{folder}'):
         run('git fetch')
-        run('git reset --hard origin/master')
+        run('git reset --hard origin/{branch}')
         run('find . -name "*.pyc" -delete')
         run('venv/bin/pip install -r requirements/live.txt')
         run('venv/bin/python manage.py syncdb')
         run('venv/bin/python manage.py migrate')
         run('venv/bin/python manage.py collectstatic --noinput')
-        run('sudo service www-mk_mmmoney restart')
+        run('sudo service {service} restart')
+
+
+@task
+def deploy():
+    deploy_styles()
+    deploy_code()
