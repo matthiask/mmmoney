d820ea6e8cdca792ac8e0c2a1d7a399fb2ea9ee4
Matthias Kestenholz <mk@feinheit.ch>
2025-06-30 10:49:55 +0200

    Update

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
 import os
-from pathlib import Path
 
 import speckenv
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
-
-BASE_DIR = Path(__file__).resolve().parent
 
 speckenv.read_speckenv()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmmoney.settings")
 application = get_wsgi_application()
-application = WhiteNoise(application)
-# TODO immutable_file_test=r"\.[0-9a-f]{12,20}\..+$"
-application.add_files(BASE_DIR / "static", "/static/")
